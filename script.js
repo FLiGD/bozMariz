953c91ad77df6504b0899959e991cea5d3c9cfe7
boz <hamedbabaeihb83@gmail.com>
2022-03-17 21:20:37 +0330

    d

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('fixed-top' , "bg-dark" );
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove
+          ("fixed-top" , "bg-dark" );
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
